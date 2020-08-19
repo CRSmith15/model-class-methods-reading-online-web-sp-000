4b473180c1182a848edb153ed6212c485cfd8548
Chance Smith <chancesmith97@hotmail.com>
2020-08-19 19:32:20 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,9 @@ class Post < ActiveRecord::Base
   def self.from_today
     where("created_at >=?", Time.zone.today.beginning_of_day)
   end
+  def self.old_news
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
 
   #put new code here
 
